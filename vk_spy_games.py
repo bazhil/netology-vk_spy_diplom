3cba844b065ca88c79f89ac6faddb5c9341aa084
Ilya Bazhenov <31971067+bazhil@users.noreply.github.com>
2018-03-18 22:32:57 +0300

    Add files via upload

--- vk_spy_games.py
+++ vk_spy_games.py
@@ -5,15 +5,16 @@ import time
 from pprint import pprint
 
 TOKEN = '5dfd6b0dee902310df772082421968f4c06443abecbc082a8440cb18910a56daca73ac8d04b25154a1128'
-# target_uid = '5030613'
-
+target_uid = '5030613'
 api_version = '5.73'
 BASE_URL = 'https://api.vk.com/method/'
 
-
-
-#получаем список друзей пользователя
 def find_friends(target_uid):
+    """
+    функция поиска друзей пользователя
+    :param target_uid: ID целевого пользователя в Вконтакте
+    :return: возвращает список идентификаторов друзей пользователя
+    """
     friends_id = []
     params = {
         'access_token': TOKEN,
@@ -23,13 +24,16 @@ def find_friends(target_uid):
 
     url = BASE_URL + 'friends.get'
     response = requests.get(url, params)
-    friends_id = response.json()['response']['items']
     if response.status_code == 200:
         friends_id = response.json()['response']['items']
     return friends_id
 
-#получаем список групп пользователя
 def find_groups(target_uid):
+    """
+    функция поиска групп пользователя
+    :param target_uid: ID целевого пользователя в Вконтакте
+    :return: возвращает словарь групп пользователя с ключами идентификатор группы значениями имя группы
+    """
     user_groups = {}
     params = {
         'access_token': TOKEN,
@@ -51,15 +55,24 @@ def find_groups(target_uid):
             pass
     return user_groups
 
-# получаем группы каждого друга пользователя
 def group_analyse(friends_id, user_groups):
+    """
+    функция анализирующая группы пользователя
+    :param friends_id: список друзей пользователя
+    :param user_groups: список групп пользователя
+    :return: возвращает список групп пользователя в которых не состоит ни один из друзей пользователя
+    """
     friend_groups = []
     for friend in friends_id:
         friend_groups += list(find_groups(friend).keys())
     return set(user_groups.keys()) - set(friend_groups)
 
-#получаем количество пользователей в группах
 def get_members(group_id):
+    """
+    функция получающая количество пользователей в группах
+    :param group_id: идентификатор группы
+    :return: возвращает количество участников группы с ключами
+    """
     users_in_group = 0
     params = {
         'access_token': TOKEN,
@@ -78,8 +91,11 @@ def get_members(group_id):
             pass
     return users_in_group
 
-#анализируем группы и сохраняем результат в файле groups.json
 def end_analyse():
+    """
+    функция анализирующая группы и сохраняющая результат в файле groups.json
+    :return:
+    """
     group_info = []
     user_id = input('Введите id целевого пользователя: ')
     if not user_id:
